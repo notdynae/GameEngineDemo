39aeba73c7ad2da7743dc508d0ad4bd6146db9aa
dynae <nightcorebanksy@gmail.com>
2024-10-31 15:46:09 -0300

    trigger in place, base projector script started

--- Assets/Scripts/ProjectorScript.cs
+++ Assets/Scripts/ProjectorScript.cs
@@ -1,8 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Projector : MonoBehaviour
 {
-    
+    public GameObject powerTrigger;
+    public GameObject redUpTrigger;
+    public GameObject redDownTrigger;
+    public GameObject greenUpTrigger;
+    public GameObject greenDownTrigger;
+    public GameObject blueUpTrigger;
+    public GameObject blueDownTrigger;
+
+    public GameObject projectorLight;
+
+    private byte _red;
+    private byte _blue;
+    private byte _green;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
 }
